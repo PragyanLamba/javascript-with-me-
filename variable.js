fadaa0856f37aacd26c300f4e7be3cf27e5191be
PRAGYAN LAMBA <115528212+PragyanLamba@users.noreply.github.com>
2024-04-20 06:48:43 +0000

    arrays study

--- variable.js
+++ variable.js
@@ -1,4 +1,20 @@
 let score = null
-console.log(typeof score) 
-let value = String(score)
-console.log(value)
+// console.log(typeof score) 
+// let value = String(score)
+// console.log(value)
+// let mycreate = new Date()
+// console.log(mycreate.toDateString())
+// console.log (mycreate.getTime())
+// let timestamp = Date.now()
+// console.log(Math.floor(timestamp/100000000))
+let newdate = new Date()
+console.log(newdate.toLocaleString('default',{weekday:'long'}))
+// in this video i learn how to use date and time 
+//date will bw ,easure in mille seconds and can be converted into seconds by divind with 1000 to avoid decimal value we use math.floor function
+//and how to manage time use locale string methods for costumiztion and we has to all wase compare the date in 
+// mille second
+const arr=[1,2,3,4,5,6,7,8,9 ]
+const arr2=[3,4,5,6,2,3,[7,6,7,5,],67,8]
+const rr=arr2.flat(Infinity)
+console.log(rr)
+//hello
