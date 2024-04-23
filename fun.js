fda3d96720138b4bc2268f79a6024c91f982685f
PRAGYAN LAMBA <115528212+PragyanLamba@users.noreply.github.com>
2024-04-23 16:24:19 +0000

    using arr in function

--- fun.js
+++ fun.js
@@ -1,5 +1,10 @@
-function me (...name){
-    return `my name is ${name} `
-}
-console.log (me('pragyan','viake','hellodady','phonk'))
+// function me (...name){
+//     return `my name is ${name} `
+// }
+// console.log (me('pragyan','viake','hellodady','phonk'))
 
+const arr=[11,22,334,4,556,66,77,44,22]
+function arryvalue(num,arrheh){
+    return arrheh[num-1]
+}
+console.log(arryvalue(3,arr))
