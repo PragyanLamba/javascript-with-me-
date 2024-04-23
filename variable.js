305b469c7d5f94ebf3bc3d7a30b65138283d50fe
PRAGYAN LAMBA <115528212+PragyanLamba@users.noreply.github.com>
2024-04-23 15:28:42 +0000

    new study of function and json

--- variable.js
+++ variable.js
@@ -1,20 +1,30 @@
-let score = null
-// console.log(typeof score) 
-// let value = String(score)
-// console.log(value)
-// let mycreate = new Date()
-// console.log(mycreate.toDateString())
-// console.log (mycreate.getTime())
-// let timestamp = Date.now()
-// console.log(Math.floor(timestamp/100000000))
-let newdate = new Date()
-console.log(newdate.toLocaleString('default',{weekday:'long'}))
-// in this video i learn how to use date and time 
-//date will bw ,easure in mille seconds and can be converted into seconds by divind with 1000 to avoid decimal value we use math.floor function
-//and how to manage time use locale string methods for costumiztion and we has to all wase compare the date in 
-// mille second
-const arr=[1,2,3,4,5,6,7,8,9 ]
-const arr2=[3,4,5,6,2,3,[7,6,7,5,],67,8]
-const rr=arr2.flat(Infinity)
-console.log(rr)
-//hello
+// let score = null
+// // console.log(typeof score) 
+// // let value = String(score)
+// // console.log(value)
+// // let mycreate = new Date()
+// // console.log(mycreate.toDateString())
+// // console.log (mycreate.getTime())
+// // let timestamp = Date.now()
+// // console.log(Math.floor(timestamp/100000000))
+// let newdate = new Date()
+// console.log(newdate.toLocaleString('default',{weekday:'long'}))
+// // in this video i learn how to use date and time 
+// //date will bw ,easure in mille seconds and can be converted into seconds by divind with 1000 to avoid decimal value we use math.floor function
+// //and how to manage time use locale string methods for costumiztion and we has to all wase compare the date in 
+// // mille second
+// const arr=[1,2,3,4,5,6,7,8,9 ]
+// const arr2=[3,4,5,6,2,3,[7,6,7,5,],67,8]
+// const rr=arr2.flat(Infinity)
+// console.log(rr)
+// //hello
+// const obj3={4:'q',5:'b'}
+// const obj4={2:'q',1:'b', 4:'pragyan'}
+// const obj2={...obj3,...obj4}
+// console.log(obj2)
+const pragyan = {
+    height:"5'10", 
+    weight:"80"
+}
+const{weight}=pragyan
+console.log(weight)
