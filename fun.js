84b8c67ac6938fef1c71a3a1bd0bffac1c976131
PRAGYAN LAMBA <115528212+PragyanLamba@users.noreply.github.com>
2024-04-24 08:54:43 +0000

    IIfe

--- fun.js
+++ fun.js
@@ -3,8 +3,16 @@
 // }
 // console.log (me('pragyan','viake','hellodady','phonk'))
 
-const arr=[11,22,334,4,556,66,77,44,22]
-function arryvalue(num,arrheh){
-    return arrheh[num-1]
-}
-console.log(arryvalue(3,arr))
+// const arr=[11,22,334,4,556,66,77,44,22]
+// function arryvalue(num,arrheh){
+//     return arrheh[num-1]
+// }
+// console.log(arryvalue(3,arr))
+// console.log(this)
+// const cahi= () =>{
+//     console.log (this)
+// }
+// cahi()
+(function(){console.log('hello')})() //example of IIFE
+
+
