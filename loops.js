6c628f1d925eaed2972043f61f9189fdb7c216b8
PRAGYAN LAMBA <115528212+PragyanLamba@users.noreply.github.com>
2024-04-27 17:37:32 +0000

    loops

---
+++ loops.js
@@ -0,0 +1,17 @@
+// for of loop 
+// const arr= [1,2,3,4,5,6,7,8,9,10]
+// for (const num of arr) {
+//     console.log(num)
+    
+// }
+// const map =new Map()
+// map.set('in',"india")
+// // // console.log(map)
+// for (const [key,value] of map) {
+//     console.log(key,':',value)
+// }
+
+
+// for (const key in map) {
+// console.log(key)
+// }
